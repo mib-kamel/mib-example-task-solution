020cebc734abff745f8d3aa11bfab4d7b2fda526
Mohamed Kamel <m.ebrahem@ymail.com>
2019-08-18 20:17:26 +0200

    Task one and task 2
    Just notice that you asked for every task in a commit after i finished
    
    Task 1
    Splitting data into segments
    
    Task 2
    Going back in time

--- client/src/map_component.js
+++ client/src/map_component.js
@@ -1,58 +1,126 @@
 /* global fetch, L */
-import React, { useEffect, useRef, useState } from 'react'
-import Moment from 'moment'
+import React, {useEffect, useRef, useState} from 'react';
+import Moment from 'moment';
+
+// new component wich will help us to pick a datetime
+import DateTimePicker from 'react-datetime-picker';
 
 const getRouteSummary = (locations) => {
-  const to = Moment(locations[0].time).format('hh:mm DD.MM')
-  const from = Moment(locations[locations.length - 1].time).format('hh:mm DD.MM')
-  return `${from} - ${to}`
+  const to = Moment(locations[0].time).format('hh:mm DD.MM');
+  const from = Moment(locations[locations.length - 1].time).format('hh:mm DD.MM');
+  return `${from} - ${to}`;
 }
 
 const MapComponent = () => {
-  const map = useRef()
-  const [locations, setLocations] = useState()
+  const map = useRef();
+  const [trips,
+    setTrips] = useState();
+  const [dateTime,
+    setDateTime] = useState();
+  const [marker,
+    setMarker] = useState();
   // Request location data.
   useEffect(() => {
+    setDateTime(new Date());
     fetch('http://localhost:3000')
       .then(response => response.json())
       .then((json) => {
-        setLocations(json)
-      })
-  }, [])
-  // TODO(Task 2): Request location closest to specified datetime from the back-end.
+        setTrips(json)
+      });
+  }, []);
 
   // Initialize map.
   useEffect(() => {
-    map.current = new L.Map('mapid')
-    const osmUrl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
-    const attribution = 'Map data © <a href="https://openstreetmap.org">OpenStreetMap</a> contributors'
+    map.current = new L.Map('mapid');
+    const osmUrl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+    const attribution = 'Map data © <a href="https://openstreetmap.org">OpenStreetMap</a> contributors';
     const osm = new L.TileLayer(osmUrl, {
       minZoom: 8,
       maxZoom: 12,
       attribution
-    })
-    map.current.setView(new L.LatLng(52.51, 13.40), 9)
-    map.current.addLayer(osm)
-  }, [])
+    });
+    map
+      .current
+      .setView(new L.LatLng(52.51, 13.40), 9);
+    map
+      .current
+      .addLayer(osm);
+  }, []);
+
   // Update location data on map.
   useEffect(() => {
-    if (!map.current || !locations) {
+    if (!map.current || !trips) {
       return // If map or locations not loaded yet.
     }
-    // TODO(Task 1): Replace the single red polyline by the different segments on the map.
-    const latlons = locations.map(({ lat, lon }) => [lat, lon])
-    const polyline = L.polyline(latlons, { color: 'red' }).bindPopup(getRouteSummary(locations)).addTo(map.current)
-    map.current.fitBounds(polyline.getBounds())
-    return () => map.current.remove(polyline)
-  }, [locations, map.current])
-  // TODO(Task 2): Display location that the back-end returned on the map as a marker.
+    // TODO(Task 1): Replace the single red polyline by the different segments on
+    // the map. to be removed in the return
+    const allPolylines = [];
+
+    // loop over thr trips and and a polyline for every trip
+    trips.forEach(trip => {
+      // generate a random number from stackover flow
+      const randomColor = '#' + Math.floor(Math.random() * (111111 + 999999) - 111111);
+
+      const latlons = trip.map(({lat, lon}) => [lat, lon]);
+
+      const polyline = L
+        .polyline(latlons, {color: randomColor})
+        .bindPopup(getRouteSummary(trip));
+
+      allPolylines.push(polyline);
+
+      polyline.addTo(map.current);
+
+      map
+        .current
+        .fitBounds(polyline.getBounds());
+    });
+
+    return () => allPolylines.forEach(polyne => {
+      map
+        .current
+        .remove(polyline);
+    });
+  }, [trips, map.current]);
+
+  // TODO(Task 2): Request location closest to specified datetime from the
+  // back-end.
+  function dateTimeSelected(dateTime) {
+    setDateTime(dateTime);
+
+    // remove the old marker
+    if (marker) {
+      map
+        .current
+        .removeLayer(marker);
+    }
+
+    if (!dateTime) {
+      return;
+    }
+
+    fetch('http://localhost:3000/location/' + dateTime.getTime())
+      .then(response => response.json())
+      .then((json) => {
+        const newMarker = L.marker([json.lat, json.lon]);
+        // to be removed if a new marker selected
+        setMarker(newMarker);
+        newMarker.addTo(map.current);
+      });
+  };
 
   return (
     <div>
-      {locations && `${locations.length} locations loaded`}
-      {!locations && 'Loading...'}
-      <div id='mapid' />
-    </div>)
+      {(trips && trips.length && trips[0] && trips[0].length) && <DateTimePicker
+        onChange={dateTimeSelected}
+        value={dateTime}
+        minDate={new Date(trips[0][0].time)}
+        maxDate={new Date()}/>}
+      {trips && ` - ${trips.length} trips loaded`}
+      {!trips && 'Loading...'}
+      <div id='mapid'/>
+    </div>
+  );
 }
 
-export default MapComponent
+export default MapComponent;
--- server/index.js
+++ server/index.js
@@ -1,5 +1,35 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+
+const exampleData = require('../data/tracking.json');
+
+// Store the timestamp in the example data object
+exampleData.forEach(data => {
+  data.timestamp = new Date(data.time).getTime();
+});
+
+// Sort the example data by the timestamp
+// It will be usefull in the search
+// Done here to do it one time over running the app - not with every request
+exampleData.sort((a, b) => a.timestamp - b.timestamp);
+
+// A function which loops over the data to closest one to the required time
+function getClosestTime(data, when) {
+  when = Number(when);
+
+  let closestTimeData = undefined;
+  let minTimestampDiff = 999999999999;
+
+  data.forEach((curData) => {
+    const currDiff = Math.abs(Number(curData.timestamp) - when);
+    if (currDiff < minTimestampDiff) {
+      minTimestampDiff = currDiff;
+      closestTimeData = curData;
+    }
+  });
+
+  return closestTimeData;
+};
 
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*')
@@ -7,16 +37,32 @@ app.use((req, res, next) => {
   next()
 })
 
-const exampleData = require('../data/tracking.json')
-
 app.get('/', (req, res) => {
-  // TODO(Task 1): Split tracking data into trip segments for example by using the time property.
-  res.send(exampleData)
+  // Generate array of trips
+  // Every trip will contain array of data points
+  // As the data is static we can do this step outside the request function run it one time
+  // But in this way it will be usefull when have different drivers data, there we can use the caching
+  const trips = [];
+
+  trips.push([]);
+  let lastTimeStamp = exampleData[0].timestamp;
+
+  exampleData.forEach(data => {
+    // if the timestamp difference is more than 20 minuts start a new trip
+    if (data.timestamp - lastTimeStamp > 20 * 60 * 1000) {
+      trips.push([]);
+    }
+    trips[trips.length - 1].push(data);
+    lastTimeStamp = data.timestamp;
+  });
+
+  res.send(trips)
 })
 
 app.get('/location/:when', (req, res) => {
-  // TODO(Task 2): Return the tracking data closest to `req.params.when` from `exampleData`.
-  res.send({})
+  const {when} = req.params;
+  const closestTime = getClosestTime(exampleData, when);
+  res.send(closestTime);
 })
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
